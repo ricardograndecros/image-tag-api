858ff0deaf535cd5ec1a4d3af20cac8f82d1d066
ricardograndecros <ricardograndecros@gmail.com>
2024-05-26 17:52:03 +0200

    Scaffold image utils

---
+++ app/utils/image_utils.py
@@ -0,0 +1,16 @@
+import requests
+
+def upload_online_image() -> str:
+    """Stores a image at imagekit.io
+
+    Returns:
+        str: the url where the image was uploaded
+    """
+    pass
+
+def delete_online_image():
+    pass
+
+def extract_tags():
+    pass
+
